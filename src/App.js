d410d3736512d9f9527a0248369cd1d7efc13e85
Nii Addo <ankamahgodson@yahoo.com>
2020-10-26 12:40:31 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React from "react";
-import Home from "./pages/Home";
-import History from "./pages/History";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import { HistoryProvider } from "./contexts/HistoryContext";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-const AppRoutes = (props) => {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/history">
-        <History />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/regsitration">
-        <Signup />
-      </Route>
-    </Switch>
-  );
-};
 
 function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <HistoryProvider>
-          <div className="App">
-            <AppRoutes />
-          </div>
-        </HistoryProvider>
-      </AuthProvider>
-    </Router>
-  );
+  return <div>blog</div>;
 }
 export default App;
